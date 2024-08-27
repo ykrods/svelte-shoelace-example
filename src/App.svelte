8243c08970219e43443f09aab48af1d3aaebf42a
ykrods <890082+ykrods@users.noreply.github.com>
2024-08-27 16:34:24 +0900

    Fix dist path and put shoelace icons

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
   import "@shoelace-style/shoelace/dist/components/button/button.js";
+  import "@shoelace-style/shoelace/dist/components/icon-button/icon-button.js";
+
+  import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+
+  setBasePath("/svelte-shoelace-example/shoelace/");
 </script>
 <div>
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
   <sl-button onclick={() => console.log("btn")}>button</sl-button>
+  <!-- svelte-ignore a11y_click_events_have_key_events -->
+  <sl-icon-button
+    name="moon"
+    label="light mode"
+    onclick={() => console.log("foo")}
+  />
 </div>
