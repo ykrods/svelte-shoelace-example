4d7db08fe18d49f2607bb3670b7d56fb026f0b47
ykrods <890082+ykrods@users.noreply.github.com>
2024-08-30 19:18:34 +0900

    Add layout component

---
+++ src/layout/SidebarContent.svelte
@@ -0,0 +1,19 @@
+<div class="sidebar-content">
+  <ul>
+    <li><a href="#Home">Home</a></li>
+    <li><a href="#Form-basic">Form (basic)</a></li>
+    <li><a href="#Form-reactive">Form (reactive)</a></li>
+    <li><a href="#Dropdown">Dropdown</a></li>
+    <li><a href="#Dialog">Dialog</a></li>
+    <li><a href="#ProgressRing">ProgressRing</a></li>
+  </ul>
+</div>
+<style>
+  ul {
+    padding: 0;
+    list-style: none;
+  }
+  .sidebar-content {
+    margin: 5rem 1rem 1rem 2rem;
+  }
+</style>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,1 +1,9 @@
-<div>Home</div>
+<script lang="ts">
+  import Layout from "$src/layout/Layout.svelte";
+</script>
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+<Layout>
+  <div>Home</div>
+</Layout>
