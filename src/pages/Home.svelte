2e3c4415ba9e4d9166c08197fc9c1cb2ef3f1c81
ykrods <890082+ykrods@users.noreply.github.com>
2024-08-31 10:12:15 +0900

    Add external link

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -5,5 +5,6 @@
   <title>Home</title>
 </svelte:head>
 <Layout>
-  <div>Home</div>
+  <h1>Home</h1>
+  <p>This is an example application of <a href="https://svelte.dev/" target="_blank">svelte</a> with <a href="https://shoelace.style/" target="_blank">shoelace</a>.</p>
 </Layout>
